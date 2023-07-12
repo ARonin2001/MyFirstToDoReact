c6881cb443967e53d5015aa65ae1f9b692c609ba
Alex <Aronin.2001@gmail.com>
2023-07-12 15:15:46 +0400

    deleted TaskCreatorFrom and create new directory name Fields. In TaskCreatorForm add new Components InputField and TextAreaField

--- src/components/TaskCreator/TaskCreator.jsx
+++ src/components/TaskCreator/TaskCreator.jsx
@@ -3,9 +3,9 @@ import TaskCreatorForm from './TaskCreatorForm/TaskCreatorForm';
 
 const TaskCreator = (props) => {
     return (
-        <div class="addTask">
-            <div class="addTask__container">
-                <div class="addTask__title title">Add a new task</div>
+        <div className="addTask">
+            <div className="addTask__container">
+                <div className="addTask__title title">Add a new task</div>
                 <TaskCreatorForm />
             </div>
         </div>
--- src/components/TaskCreator/TaskCreatorForm/TaskCreatorForm.jsx
+++ src/components/TaskCreator/TaskCreatorForm/TaskCreatorForm.jsx
@@ -1,19 +1,25 @@
+import { useState } from 'react';
 import './TaskCreatorForm.css';
-import TaskCreatorFormField from './TaskCreatorFormField/TaskCreatorFormField';
+import InputField from '../../Fields/InputField/InputField';
+import TextAreaField from './../../Fields/TextAreaField/TextAreaField';
 
 const TaskCreatorForm = (props) => {
+    const [valueTitle, setValueTitle] = useState("");
+    const [valueDate, setValueDate] = useState("");
+    const [valueTime, setValueTime] = useState("");
+    const [valueDescription, setValueDescription] = useState("");
+
     return (
-        <form class="addTask__form form form-addTask" action="#" method="post">
-            <TaskCreatorFormField fieldText={"Title"} type={"text"} />
-            <div class="field">
-                <label class="label" for="description">Description</label>
-                <div class="control">
-                    <textarea class="textarea is-hovered addTask__input" id="description" placeholder="Hovered textarea"></textarea>
-                </div>
-            </div>
-            <TaskCreatorFormField fieldText={"Date"} type={"date"} />
-            <TaskCreatorFormField fieldText={"Time"} type={"time"} />
-            <input class="button is-success is-outlined" type="submit" value="Submit a new task" />
+        <form className="addTask__form form form-addTask" action="#" method="post">
+            <InputField fieldText={"Title"} type={"text"} value={valueTitle} setValue={setValueTitle} />
+            <TextAreaField fieldText={"Title"} value={valueDescription} 
+                setValue={setValueDescription} />
+            <InputField fieldText={"Date"} type={"date"} 
+                value={valueDate} setValue={setValueDate} />
+            <InputField fieldText={"Time"} type={"time"} 
+                value={valueTime} setValue={setValueTime} />
+
+            <input className="button is-success is-outlined" type="submit" value="Submit a new task" />
         </form>
     );
 }
