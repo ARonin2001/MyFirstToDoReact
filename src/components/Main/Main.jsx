f4425acd0c347756ebf61c15418ef33b48ba9c45
Alex <Aronin.2001@gmail.com>
2023-07-17 13:43:27 +0400

    Main components who includes elements: Sort, CreateTask, Tasks

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,17 @@
+import Sort from "../Sort/Sort";
+import TaskCreator from "../TaskCreator/TaskCreator";
+import Tasks from "../Tasks/Tasks";
+
+
+const Main = (props) => {
+    return (
+        <>
+            <Sort/>
+            <TaskCreator/>
+            <Tasks/>
+        </>
+        
+    );
+}
+
+export default Main;
