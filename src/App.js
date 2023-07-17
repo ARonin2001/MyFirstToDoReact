561e7f9361ea381f31a090a443cf9b9245567ff1
Alex <Aronin.2001@gmail.com>
2023-07-17 13:41:19 +0400

    Included BrowserRouter. Components: Sort, CreateTask, Tasks moved to the Main component to draw all these elements at /

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import './reset.css';
 import './App.css';
 import Header from './components/Header/Header';
-import Sort from './components/Sort/Sort';
-import TaskCreator from './components/TaskCreator/TaskCreator';
-import Tasks from './components/Tasks/Tasks';
+import Main from './components/Main/Main';
+import AboutToDo from './components/AboutToDo/AboutToDo';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <div className="main">
-        <div className="content">
-          <div className="content__container">
-            <Sort />
-            <TaskCreator />
-            <Tasks />
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <div className="main">
+          <div className="content">
+            <div className="content__container">
+              <Routes>
+                <Route path="/" element={<Main/>}  />
+                <Route path="/about-to-do" element={<AboutToDo/>} />
+              </Routes>
+              
+            </div>
           </div>
-        </div>
-      </div> 
-    </div>
+        </div> 
+      </div>
+    </BrowserRouter>
   );
 }
 
