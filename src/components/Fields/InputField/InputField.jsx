f035157f4feb81848d4d30c91a37876f5faf31e0
Alex <Aronin.2001@gmail.com>
2023-07-21 13:07:13 +0400

    add new attribute {...props}

--- src/components/Fields/InputField/InputField.jsx
+++ src/components/Fields/InputField/InputField.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import './InputField.css';
 
-const InputField = ({fieldText, type, value, setValue}) => {
+const InputField = ({fieldText, type, value, setValue, ...props}) => {
     // const [value, setValue] = useState("");
 
     return (
@@ -9,7 +9,7 @@ const InputField = ({fieldText, type, value, setValue}) => {
             <label className="label" htmlFor={fieldText}>{fieldText}</label>
             <div className="control">
                 <input className="input addTask__input" id={fieldText} type={type} placeholder={fieldText} 
-                onChange={(e) => setValue(e.target.value)} value={value} />
+                onChange={(e) => setValue(e.target.value)} value={value} {...props} />
             </div>
         </div>
     );
